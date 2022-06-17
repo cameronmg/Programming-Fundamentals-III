8cb4a4bf994dc216292f0e933186ede985ee2440
cameronmg <101384478+cameronmg@users.noreply.github.com>
2022-06-17 11:15:37 -0500

    Create RunnerClass.java
    This is the runner class for my spell checker program.

---
+++ RunnerClass.java
@@ -0,0 +1,25 @@
+package projectOneProgramingThree;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RunnerClass {
+
+	static ArrayList increasingNumbers = (ArrayList) convertedNumbers.clone();
+	
+
+	public static void main(String[] args) throws IOException 
+	{
+		
+		
+		
+		FileNumbersExtraction.NumbersExctraction();
+		NumberOperations.setNumbersAscendingOrder(increasingNumbers);
+		NumberOperations.getNumbersAscendingOrder(increasingNumbers);
+		
+		
+	}
+	
+	
+
+}
